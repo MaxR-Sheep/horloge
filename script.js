0b2d6d60bd40cbbaaf6f8d3d9a8d00047f198f56
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-09-22 11:34:49 +0200

    probleme light mode resolu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hourHand = document.querySelector(".hour");
 const minuteHand = document.querySelector(".minute");
 const secondHand = document.querySelector(".second");
 const modeSwitch = document.querySelector(".mode-switch");
-
+const deg = 6;
 // modification texte avec changement de mode
 
 if (localStorage.getItem("mode") === "Dark Mode") {
@@ -19,19 +19,3 @@ modeSwitch.addEventListener("click", () => {
   modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
   localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
 });
-
-// fonction pour les mouvements aiguilles selon le temps
-
-const updateTime = () => {
-  let date = new date();
-  let secToDeg = (date.getSeconds() / 60) * 360;
-  let minToDeg = (date.getMinutes() / 60) * 360;
-  let hrToDeg = (date.getHours() / 12) * 360;
-
-  secondHand.style.transform = rotate({ secToDeg });
-  minuteHand.style.transform = rotate({ minToDeg });
-  hourHand.style.transform = rotate({ hrToDeg });
-};
-
-setInterval(updateTime, 1000);
-updateTime();
